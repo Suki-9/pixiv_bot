4e5770ce0fef431377165d849c17b08056ed2833
suki-9 <suki.music2004@gmail.com>
2024-09-03 20:45:13 +0900

    引数の型の検証を行ってより安全に

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -3,11 +3,17 @@ from datetime import datetime
 from typing import List, Any
 from urllib.parse import urlparse
 
-def str2time(str: str):
-  return datetime.strptime(str.split('+')[0], '%Y-%m-%dT%H:%M:%S')
+def str2time(time: str, fmt: str = '%Y-%m-%dT%H:%M:%S') -> datetime:
+  if not type(time) is str:
+    raise TypeError('間違った型の引数が与えられました')
 
-def is_url(srt: str) -> bool:
-  return urlparse(srt).scheme != ''
+  return datetime.strptime(time.split('+')[0], fmt)
+
+def is_url(url: str) -> bool:
+  if not type(url) is str:
+    raise TypeError('間違った型の引数が与えられました')
+
+  return urlparse(url).scheme != ''
 
 def is_list(list: List[Any] | Any) -> bool:
   return type(list) is List
